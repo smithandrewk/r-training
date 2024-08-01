ae035123ec903741df4e845cdd26da82e8bebbc3
Andrew Smith <andrewsmith@sc.edu>
2024-08-01 10:00:44 -0400

    lesson completed, mostly frequency tables

--- 1_Aug_2024/main.R
+++ 1_Aug_2024/main.R
@@ -1,26 +1,32 @@
-pacman::p_load(
-  rio,          # File import
-  here,         # File locator
-  skimr,        # get overview of data
-  tidyverse,    # data management + ggplot2 graphics 
-  gtsummary,    # summary statistics and tests
-  rstatix,      # summary statistics and statistical tests
-  janitor,      # adding totals and percents to tables
-  scales,       # easily convert proportions to percents  
-  flextable     # converting tables to pretty images
-)
-
+# R Lesson for August 1st, 2024
 library(readxl)
-linelist <- read_excel("r-training/1_Aug_2024/linelist_cleaned.xlsx")
+linelist <- read_excel("/Users/andrew/r-training/1_Aug_2024/linelist_cleaned.xlsx")
 View(linelist)
-
-skim(linelist)
 
 summary(linelist)
 
-linelist %>% 
-  get_summary_stats(
-    age, wt_kg, ht_cm, ct_blood, temp,  # columns to calculate for
-    type = "common")                    # summary stats to return
+summary(linelist$age)
 
-linelist %>% tabyl(age_cat)
+report <- summary(linelist$age)
+
+report["Max."]
+
+report[[6]]
+
+pacman::p_load(skimr)
+
+skim(linelist)
+
+pacman::p_load(rstatix)
+
+report <- linelist %>% get_summary_stats(type="common")
+report[c("variable","n","min","max","mean","sd")]
+
+library(janitor)
+frequency_table <- tabyl(linelist$age,show_na=FALSE)
+frequency_table['Cumulative Frequency'] <- cumsum(frequency_table$n)
+frequency_table['Cumulative Percent'] <- cumsum(frequency_table$percent)
+
+cross_tab <- linelist %>% tabyl(age,gender)
+
+linelist %>% tabyl(age,gender,hospital)
